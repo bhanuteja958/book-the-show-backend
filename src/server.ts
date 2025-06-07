88e958567b5c9655f68335e036132f69ef187e9e
bhanuteja958 <banu.teja958@gmail.com>
2025-06-07 22:16:51 +0530

    [setup]: setup basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
 import "dotenv/config";
+import express, { Express } from "express";
+const PORT = process.env.PORT;
+
+const app: Express = express();
+
+app.listen(PORT, (error?: Error) => {
+    if (error) {
+        console.error("[error]:", error?.message);
+    } else {
+        console.log(`Server running on port ${PORT}`);
+    }
+});
